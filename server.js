8e31a0a7febea2c20316f808fdf4341ba714e6b8
JohnNg-GA <john.q.ng@gmail.com>
2025-04-15 18:02:59 -0700

    added get route for pulling data from the RAWG API by gameID successfully

--- server.js
+++ server.js
@@ -20,6 +20,19 @@ app.get('/api/games', async (req, res) => {
     }
 })
 
+app.get('/api/games/:gameId', async (req, res) => {
+    const gameId = req.params.gameId
+    const url = `https://api.rawg.io/api/games/${gameId}?key=${API_KEY}`
+
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        res.json(data)
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch from RAWG API' })
+    }
+})
+
 app.listen(PORT, () => {
-    console.log(`Proxy server running on http://localhost:${PORT}`)
+    console.log(`Proxy server running on http://34.228.71.240:3060/api/games:${PORT}`)
 })
